db6220a7f18b82add3b3bf8c8a3bf79cabea5f4e
Dhruvi45 <dhruvijoshi45@gmail.com>
2023-07-02 20:44:41 +0530

    update left sidebar

--- src/components/sidebar/LeftSideBar.js
+++ src/components/sidebar/LeftSideBar.js
@@ -6,10 +6,7 @@ export default function LefftSideBar() {
   return (
     <Sidebar>
       <Menu>
-        <MenuItem className="leftMenu"> Pie charts </MenuItem>
-        <MenuItem className="leftMenu"> Line charts </MenuItem>
-        <MenuItem className="leftMenu"> Documentation </MenuItem>
-        <MenuItem className="leftMenu"> Calendar </MenuItem>
+        
       </Menu>
     </Sidebar>
   );
