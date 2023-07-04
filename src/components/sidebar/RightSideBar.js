7f0416338e1034e1080b8febed7bf43b2846d5a3
SYNOVERGETECH\joshidhr <dhruvi.joshi@synoverge.com>
2023-07-04 22:27:20 +0530

    follow and unFollow related changes

--- src/components/sidebar/RightSideBar.js
+++ src/components/sidebar/RightSideBar.js
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { axiosGet } from "../../utils/Helper";
+import { axiosGet, axiosPost } from "../../utils/Helper";
 import "./SideBar.css";
 import "./RightSideBar.css";
 import { Button, Col, Row } from "react-bootstrap";
+import { UserContext } from "../layout/Layout";
+
 export default function RightSideBar() {
+  const { user, setUser } = useContext(UserContext)
   const [userList, setUserList] = useState([]);
-  const [isFollow, setIsFollow] = useState(false);
+  const token = localStorage.getItem("token");
 
   const getUserList = async () => {
     await axiosGet(`/api/users`)
       .then((res) => {
-        setUserList(res.data.users);
+        const filteredList = res.data.users.filter((obj) => obj._id !== user._id)
+        console.log('filteredList', filteredList)
+        setUserList(filteredList);
       })
       .catch((error) => {
         console.log("error", error);
@@ -19,9 +24,25 @@ export default function RightSideBar() {
       });
   };
 
-  const follow = () => {};
+  const follow = (userId) => {
+    axiosPost(`/api/users/follow/${userId}`, {}, token)
+      .then((res) => {
+        setUser(res.data.user)
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
-  const unFollow = () => {};
+  const unFollow = (userId) => {
+    axiosPost(`/api/users/unfollow/${userId}`, {}, token)
+      .then((res) => {
+        setUser(res.data.user)
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   useEffect(() => {
     getUserList();
@@ -30,33 +51,32 @@ export default function RightSideBar() {
     <Sidebar>
       <Menu>
         {userList.length > 0 &&
-          userList.map((user, index) => {
+          userList.map((userFromUserList, index) => {
             return (
               <MenuItem
-                className={`rightMenu ${
-                  index % 2 === 0 ? "even" : "odd"
-                } pt-2 pb-2`}
+                className={`rightMenu ${index % 2 === 0 ? "even" : "odd"
+                  } pt-2 pb-2`}
               >
                 <Row>
                   <Col>
                     <Row className="followName">
                       {" "}
-                      {user.firstName} {user.lastName}{" "}
+                      {userFromUserList.firstName} {userFromUserList.lastName}{" "}
                     </Row>
-                    <Row className="followName">@ {user.username} </Row>
+                    <Row className="followName">@ {userFromUserList.username} </Row>
                   </Col>
                   <Col className="me-0">
                     <Button
                       className="followBtn mt-1"
                       onClick={() => {
-                        if (isFollow) {
-                          follow();
+                        if (!user.following.some((obj) => obj._id === userFromUserList._id)) {
+                          follow(userFromUserList._id);
                         } else {
-                          unFollow();
+                          unFollow(userFromUserList._id);
                         }
                       }}
                     >
-                      {isFollow ? "UnFollow" : "Follow"}
+                      {!user.following.some((obj) => obj._id === userFromUserList._id) ? "Follow" : "UnFollow"}
                     </Button>
                   </Col>
                 </Row>
